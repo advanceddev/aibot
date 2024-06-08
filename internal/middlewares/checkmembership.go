53791d146b6fe19b2f3b11456879f4162aa084e0
Ivan Lomovtsev <47413395+advanceddev@users.noreply.github.com>
2024-06-08 15:31:58 +0500

    FIX: middleware condition

--- internal/middlewares/checkmembership.go
+++ internal/middlewares/checkmembership.go
@@ -11,29 +11,30 @@ func CheckMembership(next tele.HandlerFunc) tele.HandlerFunc {
 	cfg := config.MustLoad()
 
 	return func(c tele.Context) error {
-		if err := checkSubscription(cfg, c); err != nil {
+		isMember, err := checkSubscription(cfg, c)
+		if err != nil {
 			return err
+		}
+
+		if !isMember {
+			menu := &tele.ReplyMarkup{RemoveKeyboard: true}
+			return c.Send("У вас нет доступа к этому боту. Свяжитесь с администратором: @frntbck", menu)
 		}
 
 		return next(c)
 	}
 }
 
-func checkSubscription(cfg *config.Config, c tele.Context) error {
+func checkSubscription(cfg *config.Config, c tele.Context) (bool, error) {
 	user := c.Recipient()
 	channel := &tele.Chat{ID: cfg.ChannelID}
-	menu := &tele.ReplyMarkup{RemoveKeyboard: true}
 
 	chatMember, err := c.Bot().ChatMemberOf(channel, user)
 	if err != nil {
-		return c.Send("Ошибка при проверке подписки: " + err.Error())
+		return false, c.Send("Ошибка при проверке подписки: " + err.Error())
 	}
 
-	if isMember(chatMember) {
-		return nil
-	}
-
-	return c.Send("У вас нет доступа к этому боту. Свяжитесь с администратором: @frntbck", menu)
+	return isMember(chatMember), nil
 }
 
 func isMember(chatMember *tele.ChatMember) bool {
