43425e8cc7997a48b30d7e7958ff4aaeb0b9bfb1
Ivan Lomovtsev <47413395+advanceddev@users.noreply.github.com>
2024-06-09 21:06:36 +0500

    Refactor chat handlers structs

--- internal/handlers/chat/types.go
+++ internal/handlers/chat/types.go
@@ -25,61 +25,47 @@ type GenAIApiResponse struct {
 	Model               string               `json:"model"`
 	Choices             []GenAIApiChoice     `json:"choices"`
 	PromptFilterResults []PromptFilterResult `json:"prompt_filter_results"`
-	Usage               struct {
-		CompletionTokens int `json:"completion_tokens"`
-		PromptTokens     int `json:"prompt_tokens"`
-		TotalTokens      int `json:"total_tokens"`
-	} `json:"usage"`
+	Usage               GenAIUsage           `json:"usage"`
+}
+
+// GenAIUsage - структура использования GenAI API
+type GenAIUsage struct {
+	CompletionTokens int `json:"completion_tokens"`
+	PromptTokens     int `json:"prompt_tokens"`
+	TotalTokens      int `json:"total_tokens"`
 }
 
 // PromptFilterResult - структура поля prompt_filter_results
 type PromptFilterResult struct {
-	PromptIndex          int `json:"prompt_index"`
-	ContentFilterResults struct {
-		Hate struct {
-			Filtered bool   `json:"filtered"`
-			Severity string `json:"severity"`
-		} `json:"hate"`
-		SelfHarm struct {
-			Filtered bool   `json:"filtered"`
-			Severity string `json:"severity"`
-		} `json:"self_harm"`
-		Sexual struct {
-			Filtered bool   `json:"filtered"`
-			Severity string `json:"severity"`
-		} `json:"sexual"`
-		Violence struct {
-			Filtered bool   `json:"filtered"`
-			Severity string `json:"severity"`
-		} `json:"violence"`
-	} `json:"content_filter_results"`
+	PromptIndex          int                       `json:"prompt_index"`
+	ContentFilterResults GenAIContentFilterResults `json:"content_filter_results"`
 }
 
 // GenAIApiChoice - структура варианта ответа
 type GenAIApiChoice struct {
-	ContentFilterResults struct {
-		Hate struct {
-			Filtered bool   `json:"filtered"`
-			Severity string `json:"severity"`
-		} `json:"hate"`
-		SelfHarm struct {
-			Filtered bool   `json:"filtered"`
-			Severity string `json:"severity"`
-		} `json:"self_harm"`
-		Sexual struct {
-			Filtered bool   `json:"filtered"`
-			Severity string `json:"severity"`
-		} `json:"sexual"`
-		Violence struct {
-			Filtered bool   `json:"filtered"`
-			Severity string `json:"severity"`
-		} `json:"violence"`
-	} `json:"content_filter_results"`
-	FinishReason string      `json:"finish_reason"`
-	Index        int         `json:"index"`
-	Logprobs     interface{} `json:"logprobs"`
-	Message      struct {
-		Content string `json:"content"`
-		Role    string `json:"role"`
-	} `json:"message"`
+	ContentFilterResults GenAIContentFilterResults `json:"content_filter_results"`
+	FinishReason         string                    `json:"finish_reason"`
+	Index                int                       `json:"index"`
+	Logprobs             interface{}               `json:"logprobs"`
+	Message              GenAIMessage              `json:"message"`
+}
+
+// GenAIContentFilterResults - структура результатов фильтрации контента
+type GenAIContentFilterResults struct {
+	Hate     GenAIFilterType `json:"hate"`
+	SelfHarm GenAIFilterType `json:"self_harm"`
+	Sexual   GenAIFilterType `json:"sexual"`
+	Violence GenAIFilterType `json:"violence"`
+}
+
+// GenAIFilterType - структура фильтрации типа контента
+type GenAIFilterType struct {
+	Filtered bool   `json:"filtered"`
+	Severity string `json:"severity"`
+}
+
+// GenAIMessage - структура сообщения
+type GenAIMessage struct {
+	Content string `json:"content"`
+	Role    string `json:"role"`
 }
